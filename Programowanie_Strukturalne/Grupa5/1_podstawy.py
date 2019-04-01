5d798c451907b844610d5b9d41fd0781e1ef6e5d
Aleksander Adamski <aleksander.adamski@edu.cdv.pl>
2019-04-01 12:42:30 +0200

    Pierwszy plik

---
+++ Programowanie_Strukturalne/Grupa5/1_podstawy.py
@@ -0,0 +1,32 @@
+print("CDV")
+print(2)
+potęga = 2 ** 10
+print(potęga)
+#potęgowanie
+imie = input()
+#konkatenacja +
+print("Twoje imie z klawiatury: " + imie)
+nazwisko = input("Podaj nazwisko:")
+print("Twoje imie z klawiatury: " + nazwisko)
+#pobieranie danych
+print()
+# Twoje imie: ... , Twoje nazwisko:...
+print("Twoje imie: " + imie + ", Twoje nazwisko: " + nazwisko)
+#gracz podaje swoj wiek, wyświetl dane w formacie: Twój wiek: ... lat
+
+print("Podaj swój wiek: ", end="")
+wiek = input() #to jest string
+print("Twój wiek: ", wiek, " lat")
+wiek1 = 20 #to jest int
+print("Twój wiek: " , wiek1,  "lat")
+nazwisko = "Kowalski"
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+dlugosc = len(nazwisko)
+print(dlugosc)
+ostatniZnak = nazwisko[len(nazwisko) - 1]
+print(ostatniZnak)
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+ostatniZnak = nazwisko[7]
+print(ostatniZnak)
